62fb059c5d4e985a94d888dd7e9eb6aef7aae66b
Lukasdotcom <lukas@lschaefer.xyz>
2022-12-04 10:36:30 -0500

    Adds time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 // The day to show (0 means all of them)
 const DAY:i32 = 0;
 mod day1;
@@ -5,6 +6,7 @@ mod day2;
 mod day3;
 mod day4;
 fn main() {
+    let now = Instant::now();
     if DAY == 0 || DAY == 1 {
         day1::main();
     }
@@ -16,5 +18,11 @@ fn main() {
     }
     if DAY == 0 || DAY == 4 {
         day4::main();
+    }
+    let elapsed_time = now.elapsed().as_micros();
+    if DAY == 0 {
+        println!("All the days took {elapsed_time} µs")
+    } else {
+        println!("Day {DAY} took {elapsed_time} µs")
     }
 }
